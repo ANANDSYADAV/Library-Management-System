136a8f17f9558f46a4a3ef9cd2ba6597df284250
anandsyadav <anandyad2004@gmail.com>
2024-02-07 23:33:46 +0530

    added signup and login routes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -4,10 +4,6 @@ const { getAllUsers, registerUser, loginUser } = require('../controllers/UserCon
 
 router.get('/getAllUsers', getAllUsers);
 router.post('/registerUser', registerUser);
-<<<<<<< HEAD
-
 router.post('/login', loginUser);
 
-=======
->>>>>>> 1912ff58b01197270e8493ee2bbaad665cec4ae4
 module.exports = router;
