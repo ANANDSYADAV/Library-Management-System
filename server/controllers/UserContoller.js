f097a945c6ecf85ebd3c167699b86467f2d4d949
Karan Raj <karanrajgodansa2003@gmail.com>
2024-02-07 23:27:58 +0530

    add login and signup route

--- server/controllers/UserContoller.js
+++ server/controllers/UserContoller.js
@@ -1,5 +1,5 @@
 const User = require('../DBModels/User');
-
+const bcrypt = require('bcrypt');
 exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.find();
@@ -11,17 +11,40 @@ exports.getAllUsers = async (req, res) => {
 
 exports.registerUser = async (req, res) => {
     const { name, email, enrollment, registration,
-        department, year, password } = req.body;
+        department, year } = req.body;
 
-    const createdUser = await User.create({
-        name,
-        email,
-        enrollment,
-        registration,
-        department,
-        year,
-        password
-    });
-
-    console.log(createdUser);
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const createdUser = await User.create({
+            name,
+            email,
+            enrollment,
+            registration,
+            department,
+            year,
+            password: hashedPassword
+        })
+        res.send(createdUser);
+    } catch (error) {
+        res.status(500).send("Error Occured");
+    }
 }
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existinguser = await User.findOne({ email });
+        if (!existinguser) {
+          return res.status(404).json({ message: "User don't Exist." });
+        }
+        const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
+        if (!isPasswordCrt) {
+          return res.status(400).json({ message: "Invalid credentials" });
+        }
+        if(isPasswordCrt){
+            return res.status(200).json({message: "login successfull"})
+        }
+    } catch {
+      res.status(500).send()
+    }
+  }
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, registerUser } = require('../controllers/UserContoller')
+const { getAllUsers, registerUser, login } = require('../controllers/UserContoller')
 
 router.get('/getAllUsers', getAllUsers);
 router.post('/registerUser', registerUser);
+router.post('/login',login);
 module.exports = router;
