3b20f84f060d2256ce12bcb1c696b962fd578da8
Karan Raj <karanrajgodansa2003@gmail.com>
2024-02-09 14:37:45 +0530

    update book cards

--- Client/src/components/Swipeit.jsx
+++ Client/src/components/Swipeit.jsx
@@ -5,25 +5,26 @@ import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import { FreeMode, Pagination } from 'swiper/modules';
 
-function Swipeit() {
+function Swipeit({ category }) {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchBookDetails = async () => {
       try {
-        const response = await fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings&offset=840');
+        const response = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(category)}&limit=50`);
         if (!response.ok) {
           throw new Error('Failed to fetch book details');
         }
         const data = await response.json();
         setBooks(data.docs); // Set the fetched book details to the state
+        console.log(data);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchBookDetails();
-  }, []); // Empty dependency array to run the effect only once
+  }, [category]); // Dependency on category to fetch books when category changes
 
   return (
     <Swiper
@@ -36,18 +37,20 @@ function Swipeit() {
       }}
       modules={[FreeMode, Pagination]}
     >
-      {books.map((book, index) => (
-        <SwiperSlide key={index} className='h-[400px] flex flex-col bg-blue-200'>
-          <img
-            src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`}
-            alt="Book Cover"
-            className='h-[350px] w-full'
-          />
-          <div className='h-[50px] w-full flex justify-center items-center cursor-pointer hover:bg-black hover:text-white'>
-            {book.title}
-          </div>
-        </SwiperSlide>
-      ))}
+      {books
+        .filter(book => book.cover_i !== undefined)
+        .map((book, index) => (
+          <SwiperSlide key={index} className='h-[400px] flex flex-col bg-blue-200'>
+            <img
+              src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`}
+              alt="Book Cover"
+              className='h-[350px] w-full'
+            />
+            <div className='h-[50px] w-full flex justify-center items-center cursor-pointer hover:bg-black hover:text-white'>
+              {book.title}
+            </div>
+          </SwiperSlide>
+        ))}
     </Swiper>
   );
 }
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -5,18 +5,18 @@ function Home() {
         <div className='h-full w-full bg-red-300 py-8 flex flex-col gap-6'>
             <div className='mx-[60px]'>
                 <div className='text-xl mb-2 font-semibold font-serif'>Computer Science</div>
-                <Swipeit />
+                <Swipeit category="Computer Science" />
             </div>
             <div className='mx-[60px]'>
                 <div className='text-xl mb-2 font-semibold font-serif'>Fiction</div>
-                <Swipeit />
+                <Swipeit category="Fiction" />
             </div>
             <div className='mx-[60px]'>
                 <div className='text-xl mb-2 font-semibold font-serif'>Health</div>
-                <Swipeit />
+                <Swipeit category="Health" />
             </div>
         </div>
     );
 }
 
-export default Home
+export default Home;
